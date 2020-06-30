31cae36d4746dc5b2372f974c335c3963a872bdc
ruuuh20 <pkim2320@gmail.com>
2020-06-30 13:02:01 -0400

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row">
+          <div className="logo-container">
+            <a href="#" className="logo">
+              LOGO
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
